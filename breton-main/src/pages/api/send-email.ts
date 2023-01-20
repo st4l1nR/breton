2dfa3c562017f4f91589cdafb751b0040a34fd7e
Kamran <Kamran>
2023-01-19 22:42:17 -0500

    send grid

--- breton-main/src/pages/api/send-email.ts
+++ breton-main/src/pages/api/send-email.ts
@@ -1,45 +1,48 @@
-import { PERMANENT_REDIRECT_STATUS } from 'next/dist/shared/lib/constants';
-import nodemailer from 'nodemailer';
+import * as sgMail from '@sendgrid/mail';
 
-export default function handler(req, res) {
+function formatContactUsEmail({ name, email, phone, message }): string {
+  return `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Contact Us</title>
+    </head>
+    <body>
+      <h1>Contact Us</h1>
+      <p>You have received a message from:</p>
+      <ul>
+        <li><strong>Name:</strong> ${name}</li>
+        <li><strong>Email:</strong> ${email}</li>
+        <li><strong>Phone:</strong> ${phone}</li>
+      </ul>
+      <p>Message:</p>
+      <p>${message}</p>
+    </body>
+  </html>
+  `;
+}
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { firstName, lastName, email, message } = req.body;
-    let mailConfig;
-    if (process.env.NODE_ENV === 'production') {
-      // all emails are delivered to destination
-      mailConfig = {
-        host: 'smtp.sendgrid.net',
-        port: 587,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS,
-        },
+    try {
+      const { firstName, lastName, email, phone, message } = req.body;
+      // Set your SendGrid API key
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      const msg = {
+        to: process.env.EMAIL,
+        from: email,
+        subject: `${firstName} ${lastName} sent you a message`,
+
+        html: formatContactUsEmail({ name: `${firstName} ${lastName}`, email, phone, message }),
       };
-    } else {
-      // all emails are catched by ethereal.email
-      mailConfig = {
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-          user: 'selena20@ethereal.email',
-          pass: 'CzQXFp5eaYgK17kWSW',
-        },
-      };
+
+      // Send the email
+      await sgMail.send(msg);
+
+      res.status(200).json({ message: 'Successfully' });
+    } catch (error) {
+      console.log(error.message)
+      res.status(500).json({ message: 'Something goes wrong' });
     }
-    let mailOptions = {
-      from: email,
-      to: 'selena20@ethereal.email',
-      subject: `${firstName} ${lastName} sent you a message`,
-      text: message,
-    };
-    let transporter = nodemailer.createTransport(mailConfig);
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        res.status(500).send(error.message);
-      } else {
-        res.status(200).json({ message: 'Successfully' });
-        console.log('Message sent: %s', info.messageId);
-      }
-    });
   }
 }
